04f8bd3cb45c52378dd44443e25b3a41cd9d433e
Jason Stevenson <jason@artofmultimedia.com.au>
2014-07-23 01:57:22 +1000

    elastic menu

--- js/main.js
+++ js/main.js
@@ -3,9 +3,22 @@
 
 
 $( document ).ready(function() {
-   
+
+   	var i = -1;
+	var menutl = new TimelineLite();
+
+
 
 	TweenLite.to($( ".menu-item" ), 0, {scale:0});
+
+
+	$( "#header li a" ).each(function( index ) {
+	  
+	  	var item = $( "#header li a" );
+	  	i++;
+	  	menutl.to(item[i], 1, {y:30, ease:Elastic.easeOut} , "-=0.9");
+	  	console.log(item)
+	});
 
 
 
@@ -16,11 +29,13 @@ $( document ).ready(function() {
 
 
 
+
+
  	$('.menu-item .banner img').mouseover(function() {
 
  		var t = $('.content');
 
-	   TweenLite.to(this, 1, {scale:1.2});
+	   TweenLite.to(this, 1, {scale:1.2, ease:Elastic.easeOut});
 
 	  })
 
@@ -28,11 +43,22 @@ $( document ).ready(function() {
 
  		var t = $('.menu-item .banner img');
 
-		TweenLite.to(t, 1, {scale:1});
+		TweenLite.to(t, 1, {scale:1, ease:Elastic.easeOut});
 
 	  })
 
 
+
+
+
+
+ 	$('#header li').mouseover(function() {
+	   TweenMax.to($("#header"), 0.8, {paddingBottom:"8em"});
+	  })
+
+ 	$('#header li').mouseleave(function() {
+ 		TweenMax.to($("#header"), 1.5,  { paddingBottom:"0em", ease:Bounce.easeOut, delay:1});
+	  })
 
 
 
