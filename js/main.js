df4ec1f38c4ecc47554493642f139a158e35aaaa
Jason Stevenson <jason@artofmultimedia.com.au>
2014-07-24 01:35:53 +1000

    grid working better

--- js/main.js
+++ js/main.js
@@ -17,48 +17,79 @@ $( document ).ready(function() {
 	  	var item = $( "#header li a" );
 	  	i++;
 	  	menutl.to(item[i], 1, {y:30, ease:Elastic.easeOut} , "-=0.9");
-	  	console.log(item)
+	
+	});
+
+	$( ".menu-item" ).each(function( index ) {
+	  
+	  	var item = $( ".menu-item" );
+	  	i++;
+	  	//menutl.to(item[i], 1, {y:30, ease:Elastic.easeOut} , "-=0.9");
+	
 	});
 
 
 
 
 	$( ".menu-item" ).each(function( index ) {
-	  TweenLite.to($( ".menu-item" ), 1, {scale:1.2, delay:1});
+	  TweenLite.to($( ".menu-item" ), 1, {scale:1.2, delay:0.5});
 	});
-
-
 
 
 
  	$('.menu-item .banner img').mouseover(function() {
 
- 		var t = $('.content');
+ 	   var info = $(this).next();
+	   TweenLite.to(this, 0.8, {scale:1.2, opacity:0.15, ease:Sine.easeInOut});
+	   TweenLite.to(info, 0.8, {x:50, ease:Sine.easeInOut});
+	   
+	   
 
-	   TweenLite.to(this, 1, {scale:1.2, ease:Elastic.easeOut});
 
 	  })
 
  	$('.menu-item').mouseleave(function() {
 
  		var t = $('.menu-item .banner img');
+		TweenLite.to(t, 0.8, {scale:1, opacity:1, ease:Sine.easeInOut});
 
-		TweenLite.to(t, 1, {scale:1, ease:Elastic.easeOut});
+		var info = $(this).find('.info');
+		TweenLite.to(info, 0.8, {x:0, ease:Sine.easeInOut});
 
+		console.log(info)
 	  })
-
-
-
-
 
 
  	$('#header li').mouseover(function() {
-	   TweenMax.to($("#header"), 0.8, {paddingBottom:"8em"});
+	   TweenMax.to($("#header"), 0.8, {paddingBottom:"8em", ease:Sine.easeInOut});
 	  })
 
- 	$('#header li').mouseleave(function() {
- 		TweenMax.to($("#header"), 1.5,  { paddingBottom:"0em", ease:Bounce.easeOut, delay:1});
+ 	$('#header').mouseleave(function() {
+ 		TweenMax.to($("#header"), 1.5,  { paddingBottom:"0em", ease:Bounce.easeOut});
 	  })
+
+
+
+
+
+ 	$('#footer').mouseover(function() {
+	   TweenMax.to(this, 1, {height:250, ease:Quad.easeOut});
+
+
+	  })
+
+ 	$('#footer').mouseleave(function() {
+ 		TweenMax.to(this, 0.8, {height:80, ease:Bounce.easeOut});
+ 
+	  })
+
+
+
+
+
+
+
+
 
 
 
